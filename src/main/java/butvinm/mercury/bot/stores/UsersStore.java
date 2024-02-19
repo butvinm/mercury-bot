c613c5ad97f862f01f8c8c763ed20c611e4d88db
Mihail Butvin <butvin.mihail@yandex.ru>
2024-02-19 05:57:00 +0300

    Better type capturing for Mongo

--- src/main/java/butvinm/mercury/bot/stores/UsersStore.java
+++ src/main/java/butvinm/mercury/bot/stores/UsersStore.java
@@ -1,12 +1,20 @@
 package butvinm.mercury.bot.stores;
 
 import java.io.File;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import butvinm.mercury.bot.telegram.models.BotUser;
 import butvinm.mercury.bot.utils.storage.Mongo;
 
 public class UsersStore extends Mongo<BotUser> {
-    public UsersStore(File store, Class<BotUser> valueType) {
-        super(store, valueType);
+    public UsersStore(File store) {
+        super(store);
+    }
+
+    @Override
+    protected TypeReference<Map<String, BotUser>> getDataType() {
+        return new TypeReference<Map<String, BotUser>>() {};
     }
 }
